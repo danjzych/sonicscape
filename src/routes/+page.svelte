d434e638c8dcbd37fb9f61d83a77ac3ee13b431d
Daniel Zych <danjzych@gmail.com>
2024-01-16 15:07:31 -0600

    Added header

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,4 +2,21 @@
 	import Sonicscape from '../components/Sonicscape.svelte';
 </script>
 
+<h1 class="color-animation text-center text-5xl font-extrabold">Sonicscape</h1>
 <Sonicscape />
+
+<style>
+	.color-animation {
+		background: radial-gradient(blue, red);
+		background-size: 200% auto;
+		-webkit-background-clip: text;
+		color: transparent;
+		animation: move-gradient 2s linear infinite;
+	}
+
+	@keyframes move-gradient {
+		to {
+			background-position: 200% center;
+		}
+	}
+</style>
